c43d3ecc068607febb083084bb83826967ce7a78
sudonetizen <sardortolibjonov1039@gmail.com>
2025-04-21 16:24:13 +0500

    add player

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     running = True
+    dt = 0
  
     while running:
         # quit 
@@ -14,13 +17,22 @@ def main():
         
         # fill screen with color
         screen.fill("black")            
+        
+        # game 
+        player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+        player.draw(screen) 
 
         # refresh screen
         pygame.display.flip()
 
-    print("Starting Asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
+        # fps 60
+        dt = clock.tick(60)/1000
+
+    #print("Starting Asteroids!")
+    #print("Screen width:", SCREEN_WIDTH)
+    #print("Screen height:", SCREEN_HEIGHT)
+    
+pygame.quit() 
 
 if __name__ == "__main__":
     main()
